c81536ebbda5226e0b8af3bde8a85aa015b1cafe
Irfan Shaikh <irfan.shaikh1@gmail.com>
2025-01-30 09:33:07 +0000

    Update ESLint configuration for ES2021 support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,19 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "module", // Allows usage of import/export
+      ecmaVersion: 2021, // Enable ES2021 features like structuredClone
+    },
+  },
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
   pluginJs.configs.recommended,
 ];
